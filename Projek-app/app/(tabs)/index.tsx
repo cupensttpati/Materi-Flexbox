f2bc18ec94bae4e0e0b02ce8d631d3e4810e9444
cupensttpati <fadhilmusyaffa073@gmail.com>
2024-11-28 07:17:16 +0700

    first commit

---
+++ Projek-app/app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, ScrollView} from 'react-native';
+import MateriFlexBox from './MateriFlexBox';
+
+const App = () => {
+  return (
+    <View>
+      <ScrollView>
+        <MateriFlexBox />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default App;
